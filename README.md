f0a43037b0ba7375fd033323f7baca5bbb095455
haucongle <leconghau.me@gmail.com>
2020-07-02 23:16:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-markdown-qsqaeq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-markdown-qsqaeq)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    MarkdownModule.forRoot(),
+  ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
